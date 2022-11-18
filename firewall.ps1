752de2bdecd851cf76d913d14263d64f97206a39
Danish Ahmed <danish.ahmed@spursol.com>
2022-11-18 18:05:59 +0500

    az fw and lb

---
+++ firewall.ps1
@@ -0,0 +1,50 @@
+$vnetname = 'TestingScriptDeleteVM-vnet-asr'
+$rg = 'tragedy'
+$location ="west US"
+$fwsubnetname = 'AzureFirewallSubnet'
+$fwprefix = '10.11.1.0/26'
+$pip = '20.237.215.134'
+
+
+#Create firewall subnet in existing vnet 
+
+$vnet = Get-AzVirtualNetwork -Name $vnetname -ResourceGroupName $rg
+Add-AzVirtualNetworkSubnetConfig -Name $fwsubnetname -AddressPrefix $fwprefix -VirtualNetwork $vnet
+$vnet | Set-AzVirtualNetwork
+
+#Creating firewall
+$Azfw = New-AzFirewall -Name vlstage-firewall -ResourceGroupName $RG -Location $Location -VirtualNetwork $vnetname -PublicIpaddress $pip
+
+#Adding outbound application rule in firewall to allow all traffic
+
+$Azfw = Get-AzFirewall -ResourceGroupName $rg
+$apprule = New-AzFirewallApplicationRule -Name 'Allow-All' -Protocol "http:80","https:443" -TargetFqdn "*" -SourceAddress '*' 
+$appruleCollection = New-AzFirewallApplicationRuleCollection -Name 'AppRuleCollection' -Priority 100 -Rule $apprule -ActionType "Allow"
+$Azfw.ApplicationRuleCollections = $appruleCollection
+Set-AzFirewall -AzureFirewall $Azfw
+
+#Adding outbound network rule in firewall to allow all traffic
+
+ $Azfw = Get-AzFirewall -ResourceGroupName $rg
+ $netrule = New-AzFirewallNetworkRule -Name 'Allow-All' -Protocol TCP -SourceAddress '*' -DestinationAddress '*' -DestinationPort "*"
+ $netrulecollection = New-AzFirewallNetworkRuleCollection -Name 'NetRuleCollection' -Priority 100 -Rule $netrule -ActionType "Allow"
+ $Azfw.NetworkRuleCollections = $netrulecollection
+ Set-AzFirewall -AzureFirewall $Azfw
+
+#Adding inbount NAT rule
+
+$Azfw = Get-AzFirewall -ResourceGroupName $rg
+$natrule1 = New-AzFirewallNatRule -Name "http" -Protocol "TCP" -SourceAddress "*" -DestinationAddress $pip -DestinationPort "80" -TranslatedAddress "10.0.0.2" -TranslatedPort "80"
+$natrule2 = New-AzFirewallNatRule -Name "https" -Protocol "TCP" -SourceAddress "*" -DestinationAddress $pip -DestinationPort "443" -TranslatedAddress "10.0.0.2" -TranslatedPort "443"
+$natruleCollection = New-AzFirewallNatRuleCollection -Name "NatRuleCollection" -Priority 1000 -Rule $natrule1, $natrule2
+$Azfw.NatRuleCollections = $natrulecollection
+Set-AzFirewall -AzureFirewall $Azfw
+
+#Creating user defined route and attach it to default subnet
+
+$Route = New-AzRouteConfig -Name "defaultroute" -AddressPrefix 0.0.0.0/0 -NextHopType VirtualAppliance -NextHopIpAddress 10.10.10.1
+New-AzRouteTable -Name "RouteTable01" -ResourceGroupName $rg -Location $location -Route $Route
+$rt = Get-AzRouteTable -ResourceGroupName $rg -Name "RouteTable01"
+$vnet = Get-AzVirtualNetwork -Name $vnetname -ResourceGroupName $rg
+Set-AzVirtualNetworkSubnetConfig  -VirtualNetwork $vnet -Name 'default' -AddressPrefix 10.11.0.0/24 -RouteTable $rt 
+Set-AzVirtualNetwork -VirtualNetwork $vnet
---
+++ lb.ps1
@@ -0,0 +1,55 @@
+$vnetname = 'TestingScriptDeleteVM-vnet-asr'
+$rg = 'tragedy'
+$location ="west US"
+
+
+$vnet = Get-AzVirtualNetwork -Name $vnetname -ResourceGroupName $rg
+$subnet = Get-AzVirtualNetworkSubnetConfig  -name 'default' -VirtualNetwork $vnet
+
+#Create FrontEndIPConfig and add the public ip to it.
+  $frontendIP = New-AzLoadBalancerFrontendIpConfig `
+  -Name 'feip' `
+  -Subnet $subnet
+
+#Create a backendAddress pool to add the virtual machines.
+  $backendPool = New-AzLoadBalancerBackendAddressPoolConfig `
+  -Name 'backendpool1'
+
+#Create the load balancer with defined variable above.
+  $lb = New-AzLoadBalancer `
+  -ResourceGroupName $rg `
+  -Name 'darpa' `
+  -Location $location `
+  -FrontendIpConfiguration $frontendIP `
+  -BackendAddressPool $backendPool
+
+#Create Load balancer Probe Config for Port 80 & 443
+  Add-AzLoadBalancerProbeConfig `
+  -Name "Port_80" `
+  -LoadBalancer $lb `
+  -Protocol tcp `
+  -Port 80 `
+  -IntervalInSeconds 5 `
+  -ProbeCount 2
+
+  Add-AzLoadBalancerProbeConfig `
+  -Name "Port_443" `
+  -LoadBalancer $lb `
+  -Protocol tcp `
+  -Port 443 `
+  -IntervalInSeconds 5 `
+  -ProbeCount 2
+
+  Set-AzLoadBalancer -LoadBalancer $lb
+
+  $probe80 = Get-AzLoadBalancerProbeConfig -LoadBalancer $lb -Name "Port_80"
+  $probe443 = Get-AzLoadBalancerProbeConfig -LoadBalancer $lb -Name "Port_443"
+
+
+# Set the Azure Load Balancer Rule Confguration for Port 80
+  $lb | Add-AzLoadBalancerRuleConfig -Name "HTTP" -FrontendIPConfiguration $lb.FrontendIpConfigurations[0] -BackendAddressPool $backendPool -Protocol "Tcp" -FrontendPort 80 -BackendPort 80 -Probe $probe80 -IdleTimeoutInMinutes 4
+  $lb | Set-AzLoadBalancer
+
+# Set the Azure Load Balancer Rule Configuration for Port 443
+  $lb | Add-AzLoadBalancerRuleConfig -Name "HTTPS" -FrontendIPConfiguration $lb.FrontendIpConfigurations[0] -BackendAddressPool $backendPool -Protocol "Tcp" -FrontendPort 443 -BackendPort 443 -Probe $probe443 -IdleTimeoutInMinutes 4
+  $lb | Set-AzLoadBalancer
